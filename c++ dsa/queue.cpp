012b5fd1ec4e2eef1622383dcb9d33730782f4a5
Anushka Sharan <anushkasharan05@gmail.com>
2025-09-22 19:42:31 +0530

    queue ds

--- c++ dsa/queue.cpp
+++ c++ dsa/queue.cpp
@@ -19,3 +19,46 @@ int main(){
     cout<<q.front()<<" :Front"<<endl;
     cout<<q.back()<<" :Back"<<endl;
 }
+
+//Implement a queue using array
+#define size 5
+class Queue{
+    int arr[size];
+    int front , rear;
+public:
+    Queue(){
+        front = -1;
+        rear = -1;
+    }
+
+    //addition from rear
+void enqueue(int val){
+    if(rear == size-1){
+        cout<<"Queue is full"<<endl;
+    }
+    cout<<arr[rear]<<endl;
+    if(front == -1 && rear == -1){
+        front = 0;
+        rear = 0;
+        arr[rear] = val;
+    }
+    else{
+        rear++;
+    }
+}
+
+    //deletion from front
+void dequeue(){
+    if(front == -1 && rear == -1){
+        cout<<"Queue is empty"<<endl;
+    }
+    cout<<arr[front]<<endl;
+    if(front == rear){
+        front = -1;
+        rear = -1;
+    }
+    else{
+        front++;
+    }
+}
+};  
